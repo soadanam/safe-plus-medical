f51cc441bc2d49fbc7443a96ea27241cf5d64a85
soadam <soad.anam7@gmail.com>
2022-11-24 12:10:30 +0600

    changed some style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -55,8 +55,6 @@ const Header = () => {
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-
-
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
